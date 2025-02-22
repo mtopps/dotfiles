2a10f860cf28d5a715d421897e7b8d83bbfbe518
Matthew Topping <matthewatopping@gmail.com>
2025-02-23 12:49:53 +1300

    update dotfiles

---
+++ dot_zsh/aliases.zsh
@@ -0,0 +1,33 @@
+# Terraform
+alias tf='terraform'
+alias tff='terraform fmt -recursive'
+alias tfp='terraform plan'
+alias tfsl='terraform state list'
+alias tfss='terraform state show'
+alias tfaa="terraform apply -auto-approve"
+
+# git 
+alias gitcm='git commit -m'
+alias gitdir="git rev-parse --show-toplevel"
+
+# gcloud
+alias gcelist="gcloud compute instances list"
+alias gcal="gcloud_account"
+
+# azcli
+alias azlogin='az login --use-device-code'
+alias az_mt='az account set --subscription mtopps'
+
+# Basic
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias zshrc='source ~/.zshrc'
+
+alias ll='ls -la'
+alias llt='ls -ltra'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias history="history 1"
+
---
+++ dot_zsh/export.zsh
@@ -0,0 +1,14 @@
+# Colorize ls
+export CLICOLOR=1
+
+# History configurations
+export HISTFILE="$HOME/.zsh_history"
+export HISTSIZE="100000"
+export SAVEHIST="$HISTSIZE"
+
+# Docker
+export DOCKER_HOST="unix://$HOME/.colima/docker.sock"
+
+# Homebrew
+export HOMEBREW_NO_AUTO_UPDATE=1
+#export HOMEBREW_NO_INSTALL_FROM_API=1
