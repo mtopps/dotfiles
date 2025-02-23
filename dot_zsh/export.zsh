e981225fd2c78d2dc4b4038d9f84c75a65152143
Matthew Topping <matthewatopping@gmail.com>
2025-02-23 16:30:25 +1300

    Update .zsh/export.zsh

--- dot_zsh/export.zsh
+++ dot_zsh/export.zsh
@@ -6,9 +6,6 @@ export HISTFILE="$HOME/.zsh_history"
 export HISTSIZE="100000"
 export SAVEHIST="$HISTSIZE"
 
-# Docker
-export DOCKER_HOST="unix://$HOME/.colima/docker.sock"
-
 # Homebrew
 export HOMEBREW_NO_AUTO_UPDATE=1
 #export HOMEBREW_NO_INSTALL_FROM_API=1
