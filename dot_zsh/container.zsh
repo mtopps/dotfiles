cc598d3dda7cda942eee839410933e4ca8913bdf
Matthew Topping <m@mtopps.xyz>
2026-08-13 08:42:09 +1200

    Remove .zsh/container.zsh

--- dot_zsh/container.zsh
+++
@@ -1,8 +0,0 @@
-# Container system management
-# Auto-start container system if not already running
-
-if command -v container &>/dev/null; then
-    if ! container system status &>/dev/null || container system status | grep -q "not running"; then
-        container system start &>/dev/null
-    fi
-fi
